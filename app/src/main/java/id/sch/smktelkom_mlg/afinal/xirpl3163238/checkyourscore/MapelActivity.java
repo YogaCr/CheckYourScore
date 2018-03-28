bd0eeed84da9dea73c2a13602c59738ef6b60cdd
ferlinards <ferlina_firdausi_25rpl@student.smktelkom-mlg.sch.id>
2018-03-28 13:45:28 +0700

    1. membuat dan mengatur layout dari tampilan nilai mata pelajaran

---
+++ app/src/main/java/id/sch/smktelkom_mlg/afinal/xirpl3163238/checkyourscore/MapelActivity.java
@@ -0,0 +1,17 @@
+package id.sch.smktelkom_mlg.afinal.xirpl3163238.checkyourscore;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Ferlina Firdausi on 28/03/2018.
+ */
+
+public class MapelActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mapel);
+    }
+}
