403ca63e494a0133c7e6a89b86c9d22c2ef5bc33
Sakata Yoga <yogacahya08@gmail.com>
2018-04-24 20:10:50 +0700

    Menambahkan Fragment untuk statistik guru

---
+++ app/src/main/java/id/sch/smktelkom_mlg/afinal/xirpl3163238/checkyourscore/StatistikGuruFragment.java
@@ -0,0 +1,29 @@
+package id.sch.smktelkom_mlg.afinal.xirpl3163238.checkyourscore;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class StatistikGuruFragment extends Fragment {
+
+
+    public StatistikGuruFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_statistik_guru, container, false);
+    }
+
+}
