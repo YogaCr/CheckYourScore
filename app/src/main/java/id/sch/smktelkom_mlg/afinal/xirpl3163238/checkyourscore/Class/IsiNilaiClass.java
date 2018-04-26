595e2a0be051c48cb28230179af1f0c818aa9a48
sadaa <sada62510@gmail.com>
2018-04-26 18:07:11 +0700

    -Membuat isi nilai guru dan daftar remidi

---
+++ app/src/main/java/id/sch/smktelkom_mlg/afinal/xirpl3163238/checkyourscore/Class/IsiNilaiClass.java
@@ -0,0 +1,26 @@
+package id.sch.smktelkom_mlg.afinal.xirpl3163238.checkyourscore.Class;
+
+/**
+ * Created by sadaa on 4/26/2018.
+ */
+
+public class IsiNilaiClass {
+    private String NamaSiswa;
+    private Double NilaiSiswa;
+
+    public String getNamaSiswa() {
+        return NamaSiswa;
+    }
+
+    public void setNamaSiswa(String namaSiswa) {
+        this.NamaSiswa = namaSiswa;
+    }
+
+    public Double getNilaiSiswa() {
+        return NilaiSiswa;
+    }
+
+    public void setNilaiSiswa(Double nilaiSiswa) {
+        this.NilaiSiswa = nilaiSiswa;
+    }
+}
---
+++ app/src/main/java/id/sch/smktelkom_mlg/afinal/xirpl3163238/checkyourscore/adapter/IsiNilaiAdapter.java
@@ -0,0 +1,60 @@
+package id.sch.smktelkom_mlg.afinal.xirpl3163238.checkyourscore.adapter;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.List;
+
+import id.sch.smktelkom_mlg.afinal.xirpl3163238.checkyourscore.Class.IsiNilaiClass;
+import id.sch.smktelkom_mlg.afinal.xirpl3163238.checkyourscore.R;
+
+/**
+ * Created by sadaa on 4/26/2018.
+ */
+
+public class IsiNilaiAdapter extends RecyclerView.Adapter<IsiNilaiAdapter.ViewHolder> {
+    Context context;
+    List<IsiNilaiClass> isiNilaiClassList;
+
+    public IsiNilaiAdapter(List<IsiNilaiClass> isiNilaiClassList, Context context) {
+        this.isiNilaiClassList = isiNilaiClassList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public IsiNilaiAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(context).inflate(R.layout.activity_isi_nilai_guru, parent, false);
+        return new ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull IsiNilaiAdapter.ViewHolder holder, int position) {
+        holder.NamaSiswa.setText(isiNilaiClassList.get(position).getNamaSiswa());
+        holder.NilaiSiswa.setText(String.valueOf(isiNilaiClassList.get(position).getNilaiSiswa()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return isiNilaiClassList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView NamaSiswa;
+        EditText NilaiSiswa;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            NamaSiswa = itemView.findViewById(R.id.tvNamaSiswa);
+            NilaiSiswa = itemView.findViewById(R.id.edNilaiSiswa);
+        }
+    }
+}
