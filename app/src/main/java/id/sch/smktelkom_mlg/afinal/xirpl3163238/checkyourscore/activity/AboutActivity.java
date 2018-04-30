326bd696f9d29754de0f70b5709a32ee8732d3b0
Sakata Yoga <yogacahya08@gmail.com>
2018-04-30 14:05:53 +0700

    Memperbaiki about

--- app/src/main/java/id/sch/smktelkom_mlg/afinal/xirpl3163238/checkyourscore/activity/AboutActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/afinal/xirpl3163238/checkyourscore/activity/AboutActivity.java
@@ -1,18 +1,14 @@
 package id.sch.smktelkom_mlg.afinal.xirpl3163238.checkyourscore.activity;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
 import id.sch.smktelkom_mlg.afinal.xirpl3163238.checkyourscore.R;
 
-/**
- * Created by Ferlina Firdausi on 29/04/2018.
- */
-
 public class AboutActivity extends AppCompatActivity {
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
     }
